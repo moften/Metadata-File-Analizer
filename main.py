f7ac4ffcb40dac441a1a091629fc5bab337eed8b
Francisco Santibañez <ing.fco.ls@gmail.com>
2025-07-14 19:17:21 -0600

    input

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# main.py
-
-import os
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import PathCompleter
 from banner import show_banner
-from scanner import select_path, scan_metadata
+from scanner import scan_metadata
 from cleaner import clean_metadata
+import os
+
+def clean_path(path: str) -> str:
+    return os.path.expanduser(path.strip().replace('\\ ', ' '))
 
 def main():
     show_banner()
@@ -13,14 +16,14 @@ def main():
         print("2. Eliminar metadatos")
         print("3. Salir")
 
-        choice = input("\nSelecciona una opción (1/2/3): ")
+        choice = prompt("\nSelecciona una opción (1/2/3): ")
 
         if choice == "1":
-            path = select_path()
-            scan_metadata(path)
+            path = prompt("Introduce la ruta del archivo o carpeta a procesar: ", completer=PathCompleter())
+            scan_metadata(clean_path(path))
         elif choice == "2":
-            path = select_path()
-            clean_metadata(path)
+            path = prompt("Introduce la ruta del archivo o carpeta a procesar: ", completer=PathCompleter())
+            clean_metadata(clean_path(path))
         elif choice == "3":
             print("¡Hasta pronto, hacker del metadato!")
             break
